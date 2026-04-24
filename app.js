a6fd79c87fec594317e08036777d9e3e6346db04
maxkrcl <maxim.krupanov@gmail.com>
2026-04-24 23:46:07 +0300

    deploy test 2

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/page2', (req, res) => {
   res.send('Page 2');
 });
 
+app.get('/page3', (req, res) => {
+  res.send('Страница 3');
+});
+
 app.listen(3030, () => {
   console.log('http://localhost:3030/');
 });
